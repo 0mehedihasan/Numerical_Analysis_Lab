e8911a83fa63c2c8959eeda1fbcf7aa2d32dd0e4
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2023-08-01 13:58:02 +0600

    new

--- lab-04/false_position.m
+++ lab-04/false_position.m
@@ -1,29 +1,34 @@
-f = @(x) (x^3)-(0.165*(x^2))+3.993*(10^(-4));
+f = @(x) x^3-0.165*x^2+3.993*10^-4;
 xl = input("Enter lower bound: ");
 xu = input("Enter Upper bound: ");
 e = input("Enter Error tollerance: ");
-xm=(xl+xu)/2;
+fl= f(xl);
+fu= f(xu);
+xr=(xu*fl - xl*fu)/(fl - fu);
+fr=f(xr);
+
 it=1;
 
 printf("\nIteration : %d \n", it);
 printf("XL = %d\n", xl);
 printf("XU = %d\n", xu);
-printf("XM = %d\n", xm);
+printf("XR = %d\n", xr);
 printf("Relative approximate error: ... \n");
 it = it+1;
 while(1)
+   
+   
+   if (fl*fr < 0)
+      xu=xr;
+   else
+      xl=xr;
+   endif
    fl= f(xl);
    fu= f(xu);
-   fm= f(xm);
-   if (fl*fm < 0)
-      xu=xm;
-   else
-      xl=xm;
-   endif
-
-   oxm = xm;
-   xm=(xl+xu)/2;
-   error =abs((xm - oxm)/xm)*100 ;
+   fr=f(xr);
+   oxr = xr;
+   xr=(xu*fl - xl*fu)/(fl - fu);
+   error =abs((xr - oxr)/xr)*100 ;
    if(error <=e)
       break;
    endif
@@ -32,7 +37,7 @@ while(1)
    printf("\nIteration : %d \n", it);
    printf("XL = %d\n", xl);
    printf("XU = %d\n", xu);
-   printf("Root (XM) = %d\n", xm);
+   printf("Root (XR) = %d\n", xr);
    printf("Relative approximate error: %d \n", error);
 
    it = it+1;
